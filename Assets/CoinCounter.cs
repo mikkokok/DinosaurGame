fa2dffa5b32feba6144d8333864a874621706402
Mikko Kokkonen <mikko.kokkonen@olen.to>
2016-10-02 12:40:53 +0300

    Few test screens, added coins and few game mechanics

---
+++ Assets/CoinCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinCounter : MonoBehaviour
+{
+
+    public static int coincount;
+
+    public static void Addcoin () {
+        coincount++;
+        //Debug.Log("coincount: "+coincount ); // For debugging
+        if (coincount == 10)
+        {
+            Destroy(GameObject.FindWithTag ("Car"));
+        }
+    }
+    public static string GetCoin () {
+        return coincount.ToString();
+    }
+} // CoinCounter
--- Assets/Scripts/Dinosaur2Move.cs
+++ Assets/Scripts/Dinosaur2Move.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Dinosaur2Move : MonoBehaviour {
 
     private float gameSpeed = 5;
-    private float maxSpeed = 10;
+    private float maxSpeed = 30;
 	// Use this for initialization
 	void Start ()
     {
@@ -22,7 +22,7 @@ public class Dinosaur2Move : MonoBehaviour {
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
             if (gameSpeed < maxSpeed) { 
-            gameSpeed = gameSpeed * 2;
+            gameSpeed = gameSpeed * 3;
             }
         }
         if (Input.GetKey (KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
@@ -35,18 +35,34 @@ public class Dinosaur2Move : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(Vector2.up * gameSpeed * Time.deltaTime);
-        }
+            transform.Translate(Vector2.up * gameSpeed * Time.deltaTime * 2);
+         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector2.down * gameSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
         }
     } // movement
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Finish")
         {
-            Debug.Log("You win!");
+            endgame("You win! Press esc to quit");
+            //Debug.Log("You win!");
+        }
+        if (col.gameObject.tag == "Border")
+        {
+            endgame("You lose! Press esc to quit");
+            //Debug.Log("You win!");
         }
     }
+    private void endgame (string input)
+    {
+        UpdateInfo.game_end = true;
+        UpdateInfo.UpdateText(input);
+    }
+
 }
