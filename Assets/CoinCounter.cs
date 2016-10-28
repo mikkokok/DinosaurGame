bbf77a238d79122abe96c4fc5fd7b6e575f48b1b
unknown <pamama@utu.fi>
2016-10-28 15:55:35 +0300

    coincounter

--- Assets/CoinCounter.cs
+++ Assets/CoinCounter.cs
@@ -14,6 +14,19 @@ public class CoinCounter : MonoBehaviour
             Destroy(GameObject.FindWithTag ("Car"));
         }
     }
+	public static void Losecoin (){
+		if(coincount == 0) return;
+		coincount = coincount -1;
+	}
+	public static void Losecoin (int m){
+		if(coincount - m < 0){
+			coincount = 0;
+		}
+		else coincount = coincount -m;
+	}
+
+
+
     public static string GetCoin () {
         return coincount.ToString();
     }
