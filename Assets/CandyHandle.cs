fc99b846199c692f7cebfc6c1805518ff96e3ea7
Mikko <Mikko>
2016-11-06 11:52:57 +0200

    Added few candies

---
+++ Assets/CandyHandle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyHandle : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            //Debug.Log("Coin found"); // For debugging
+            CoinCounter.Addcoin();
+        }
+    }
+}
--- Assets/CoinCounter.cs
+++ Assets/CoinCounter.cs
@@ -6,35 +6,45 @@ public class CoinCounter : MonoBehaviour
 
     public static int coincount;
 
-    public static void Addcoin () {
+
+    public static void Addcoin()
+    {
         coincount++;
         //Debug.Log("coincount: "+coincount ); // For debugging
         if (coincount == 10)
         {
-           Destroy(GameObject.FindWithTag ("Car"));
+            Destroy(GameObject.FindWithTag("Car"));
+        }
+    }
+    public static void AddCandy()
+    {
+        Debug.Log("Candy collected"); // For debugging
+    }
+
+    public static void AddSilvercoin()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Addcoin();
         }
     }
 
-public static void AddSilvercoin () {
-	for(int i =0; i<5;i++){
-		Addcoin();
-		}
-	}
+    public static void Losecoin()
+    {
+        if (coincount == 0) return;
+        coincount = coincount - 1;
+    }
+    public static void Losecoin(int m)
+    {
+        if ((coincount - m) < 0)
+        {
+            coincount = 0;
+        }
+        else coincount = coincount - m;
+    }
 
-public static void Losecoin (){
-		if(coincount == 0) return;
-		coincount = coincount -1;
-	}
-	public static void Losecoin (int m){
-		if((coincount - m) < 0){
-			coincount = 0;
-		}
-		else coincount = coincount -m;
-	}
-
-
-
-    public static string GetCoin () {
+    public static string GetCoin()
+    {
         return coincount.ToString();
     }
 } // CoinCounter
