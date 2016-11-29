b4e78ac5882e190ce44d468321a4943e239042bc
Tinglemies <pamama@utu.fi>
2016-11-29 18:12:44 +0200

    Car now changes direction when it touches checkpoint

--- Assets/MoveCar.cs
+++ Assets/MoveCar.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class MoveCar : MonoBehaviour {
+	int dir // -1 = goes to left, 1 goes to right
 
 	// Use this for initialization
 	void Start () {
-	
+	dir = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.left * 100 * Time.deltaTime);
+        transform.Translate(Vector2.left * 100 * -1 * dir * Time.deltaTime);
     }
+	 void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Checkpoint"){
+            dir = dir * -1;
+}
 }
